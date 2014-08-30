7175fe8e4c570088b6aebb849445814dc36414bd
Hakimu Jackson <hakimu@newrelic.com>
2014-08-30 00:25:21 -0700

    Add middleware and 404 page

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var path = require('path');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req,res){
@@ -13,6 +17,10 @@ app.get('/', function(req,res){
 
 app.get('/:a?/:b?/:c?', function(req,res){
 	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+});
+
+app.use(function(req,res){
+	res.render('404', {url:req.url});
 });
 
 http.createServer(app).listen(app.get('port'), function(){
